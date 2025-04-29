1ad99f134e589bbf764ad4b9f436e4107b80d1b4
KirichenkoDm <kir2dm@gmail.com>
2025-04-29 19:28:18 +0300

    Remove migration imports

--- backend/src/database/dataSource.ts
+++ backend/src/database/dataSource.ts
@@ -2,12 +2,6 @@ import "reflect-metadata";
 import { DataSource } from "typeorm";
 import * as dotenv from "dotenv";
 import { ColumnEntity, CommentEntity, ProjectEntity, RoleEntity, TaskEntity, UserEntity } from "../resourses";
-import { UsersInsert1742896192584 } from "./migrations/1742896192584-UsersInsert";
-import { ProjectInsert1742910651015 } from "./migrations/1742910651015-ProjectInsert";
-import { RoleInsert1742923160383 } from "./migrations/1742923160383-RoleInsert";
-import { ColumnInsert1742924639407 } from "./migrations/1742924639407-ColumnInsert";
-import { TaskInsert1742926783780 } from "./migrations/1742926783780-TaskInsert";
-import { CommentInsert1742928923624 } from "./migrations/1742928923624-CommentInsert";
 
 dotenv.config();
 
@@ -28,6 +22,6 @@ export const AppDataSource = new DataSource({
     TaskEntity,
     CommentEntity,
   ],
-  migrations: [CommentInsert1742928923624],
+  migrations: [],
   subscribers: [],
 });

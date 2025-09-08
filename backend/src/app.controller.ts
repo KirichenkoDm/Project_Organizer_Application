09aa342e17326b5d370e68a169ba37e1d0dec8bb
DmyrtoKyrychenko <dmytro.kyrychenko@tech-stack.io>
2025-09-08 16:59:58 +0300

    Make health check public

--- backend/src/app.controller.ts
+++ backend/src/app.controller.ts
@@ -1,7 +1,9 @@
 import { Controller, Get } from '@nestjs/common';
+import { Public } from './shared/public.decorator';
 
 @Controller()
 export class AppController {
+  @Public()
   @Get()
   getRoot() {
     return { status: 'ok' };

70564f40674da623c2bd14e28a23687f52584eb0
DmyrtoKyrychenko <dmytro.kyrychenko@tech-stack.io>
2025-09-08 17:07:59 +0300

    No roles for health check

--- backend/src/app.controller.ts
+++ backend/src/app.controller.ts
@@ -1,9 +1,11 @@
 import { Controller, Get } from '@nestjs/common';
 import { Public } from './shared/public.decorator';
+import { SkipRoles } from './shared/roles.decorator';
 
 @Controller()
 export class AppController {
   @Public()
+  @SkipRoles()
   @Get()
   getRoot() {
     return { status: 'ok' };

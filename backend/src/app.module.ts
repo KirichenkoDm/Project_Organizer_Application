04107bf7ef6647b5d707ad4f0ae8885627abbf15
DmyrtoKyrychenko <dmytro.kyrychenko@tech-stack.io>
2025-09-08 16:54:57 +0300

    App controller for health check

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,10 +1,11 @@
 import { MiddlewareConsumer, Module, NestModule } from "@nestjs/common";
 import * as cookieParser from "cookie-parser";
+import { AppController } from "./app.controller";
 import { appImports, appProviders } from "./joined";
 
 @Module({
   imports: appImports,
-  controllers: [],
+  controllers: [AppController],
   providers: appProviders
 })
 export class AppModule implements NestModule {
